7d6c48f29ca42f3c2b0a7c9090cd8008e8406896
Mariapia Gianguzzi <piagianguzzi@gmail.com>
2021-01-11 16:34:24 +0100

    vue-dischi refactoring

--- main.js
+++ main.js
@@ -9,15 +9,14 @@ $(document).ready(function () {
   let app = new Vue({
     el: "#app",
     data: {
-      listaCd: [],
+      listaCd: "",
     },
     mounted() {
       axios
         .get("https://flynn.boolean.careers/exercises/api/array/music")
         .then((response) => {
-          console.log(response.data.response);
           let cd = response.data.response;
-          this.listaCd.push(cd);
+          this.listaCd = cd;
         });
     },
   });
